604a05e24e88bb908cbd31d135b7682a68b1dbfc
Tigran Kirakosov <tkirakosov@mail.ru>
2020-09-03 17:24:31 +0300

    Added user control

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
-import { Engine, Scene, FreeCamera, HemisphericLight, Vector3, MeshBuilder } from '@babylonjs/core';
+import {
+  Engine,
+  Scene,
+  FreeCamera,
+  HemisphericLight,
+  Vector3,
+  MeshBuilder,
+  StandardMaterial,
+  Color3
+} from '@babylonjs/core';
+import PlayerController from './player/player.controller';
 
 class Game {
-  private _canvas: HTMLCanvasElement;
-  private _engine: Engine;
-  private _scene: Scene;
-  private _camera: FreeCamera;
-  private _light: HemisphericLight;
-  private readonly _methodToEventMap: { [key: string]: EventListenerOrEventListenerObject[] };
+  private canvas: HTMLCanvasElement;
+  private engine: Engine;
+  private scene: Scene;
+  private camera: FreeCamera;
+  private light: HemisphericLight;
+  private player: PlayerController;
+  public unregisterEventListeners: Function;
 
   constructor(canvasElement: string) {
-    this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
-    this._engine = new Engine(this._canvas, true);
-    let x = 0;
-    this._methodToEventMap = {
-      'keydown': [
-        (e) => { console.log(x--) } 
-      ],
-      'resize': [
-        (e) => this._engine.resize()
-      ],
-      'mousewheel': [
-        (e) => { console.log(x++)} 
-      ]
-    };
+    this.canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+    this.engine = new Engine(this.canvas, true);
   }
 
-  static init(): Game {
+  static async init(): Promise<Game> {
     const game = new Game('renderCanvas');
-    window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('DOMContentLoaded', async () => {
 
-      const unregisterEventListeners = game.registerEventListeners();
-
-      game.createScene();
+      await game.createScene();
+      
+      game.unregisterEventListeners = await game.registerEventListeners();
 
       game.doRender();
 
@@ -39,42 +38,61 @@ class Game {
     return game;
   }
 
-  createScene(): void {
-    this._scene = new Scene(this._engine);
+  async createScene(): Promise<void> {
 
-    this._camera = new FreeCamera('camera1', new Vector3(0, 5,-10), this._scene);
+    this.scene = new Scene(this.engine);
 
-    this._camera.setTarget(Vector3.Zero());
+    const myMaterial = new StandardMaterial("myMaterial", this.scene);
 
-    this._camera.attachControl(this._canvas, false);
+    myMaterial.diffuseColor = new Color3(1, 0, 1);
+    myMaterial.specularColor = new Color3(0.5, 0.6, 0.87);
+    myMaterial.ambientColor = new Color3(0, 2, 1);
+    myMaterial.emissiveColor = new Color3(0, 2, 1);
 
-    this._light = new HemisphericLight('light1', new Vector3(0,1,0), this._scene);
+    this.camera = new FreeCamera('camera1', new Vector3(0, 5,-10), this.scene);
 
-    const sphere = MeshBuilder.CreateSphere('sphere',
-                                { segments: 16, diameter: 4 }, this._scene);
+    this.camera.setTarget(Vector3.Zero());
+
+    this.camera.attachControl(this.canvas, false);
+
+    this.light = new HemisphericLight('light1', new Vector3(0,1,0), this.scene);
+    this.light.intensity = 0.6;
+
+    const sphere = MeshBuilder.CreateSphere('sphere', { segments: 16, diameter: 1 }, this.scene);
 
     sphere.position.y = 1;
+    
+    sphere.material = myMaterial;
+
+    this.player = new PlayerController(sphere);
 
     const ground = MeshBuilder.CreateGround('ground',
-                                { width: 10, height: 6, subdivisions: 2 }, this._scene);
+                                { width: 10, height: 6, subdivisions: 2 }, this.scene);
+
+                                console.log(this.scene.meshes.map(({ id }) => id))
   }
 
-  doRender(): void {
-    this._engine.runRenderLoop(() => {
-      this._scene.render();
+  doRender(): void { this.engine.runRenderLoop(() => this.scene.render()); }
+
+  async registerEventListeners(): Promise<Function> {
+    const eventMethodPairs = Object.entries({
+      'keydown': [this.player.inputListener],
+      'resize': [
+        (e) => this.engine.resize()
+      ],
+      'mousewheel': [
+        (e) => { } 
+      ]
     });
-  }
 
-  registerEventListeners(): Function {
-    const pairs = Object.entries(this._methodToEventMap);
-    for(const [event, methods] of pairs) {
+    for(const [event, methods] of eventMethodPairs) {
       methods.forEach(method => {
         window.addEventListener(event, method);
       });
     }
 
     return () => {
-      for(const [event, methods] of pairs) {
+      for(const [event, methods] of eventMethodPairs) {
         methods.forEach(method => {
           window.removeEventListener(event, method);
         });
@@ -83,4 +101,6 @@ class Game {
   }
 }
 
-const game = Game.init();
+let game: Game;
+
+Game.init().then(g => game = g);
